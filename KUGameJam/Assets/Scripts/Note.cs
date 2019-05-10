924bce5934e31aa74838846b12963a704436e804
ryane3000 <ryane3000@gmail.com>
2019-05-10 20:47:47 +0100

    Chris is a twat

---
+++ KUGameJam/Assets/Scripts/Note.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note : MonoBehaviour
+{
+
+    public Image Noteimage;
+
+    public AudioClip pickupSound;
+    public AudioClip putawaySound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Noteimage.enabled = false;
+    }
+
+    public void ShowNoteImage()
+    {
+        Noteimage.enabled = true;
+        GetComponent<AudioSource>().PlayOneShot(pickupSound);
+    }
+
+    public void HideNoteImage()
+    {
+        Noteimage.enabled = false;
+        GetComponent<AudioSource>().PlayOneShot(putawaySound);
+    }
+}
