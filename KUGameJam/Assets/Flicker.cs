2c9335b9613676ae36dfcce25c4b819c91602cea
ryane3000 <ryane3000@gmail.com>
2019-05-11 07:35:58 +0100

    Game Design

---
+++ KUGameJam/Assets/Flicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+
+    
+
+{
+    Light light;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing ()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            light.enabled = !light.enabled;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
